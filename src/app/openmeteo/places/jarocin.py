c0600295ce1329be4b36dbfb0fa7ba41c867619c
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-20 18:56:13 +0200

    add comments

--- src/app/openmeteo/places/jarocin.py
+++ src/app/openmeteo/places/jarocin.py
@@ -1,3 +1,22 @@
 LATITUDE: float = 51.9727
 LONGITUDE: float = 17.5026
 TIMEZONE: str = "Europe/Berlin"
+
+
+# latitude=51.9727 ; longitude=17.5026 ; Jarocin
+
+# Sea
+# latitude=53.9105 ; longitude=14.2471 ; Świnoujście
+# latitude=54.2609 ; longitude=16.0621 ; Mielno
+# latitude=54.761 ; longitude=17.5555 ; Łeba
+# latitude=54.6038 ; longitude=18.8035 ; Hel
+# latitude=54.3523 ; longitude=18.6491 ; Gdańsk
+
+# Mountain towns
+# latitude=50.7767 ; longitude=15.7559 ; Karpacz
+# latitude=49.299 ; longitude=19.9489 ; Zakopane
+
+# Mountain peaks
+# latitude=49.1791 ; longitude=20.0884 ; Rysy
+# latitude=49.2509 ; longitude=19.9343 ; Giewont
+# latitude=50.7361 ; longitude=15.7398 ; Śnieżka
