15d8f3f6e6119561075eec63047192934d23e94c
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-20 14:03:41 +0200

    rename to context

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,7 @@ import structlog
 from fastapi import APIRouter, Response
 from fastapi.responses import FileResponse
 
-from src.api.time_provider.time_provider import default_time_sync_context
+from src.api.time_provider.context import default_time_sync_context
 from src.api.time_provider.time_sync_context import TimeSyncContext
 
 logger = structlog.get_logger(__name__)
