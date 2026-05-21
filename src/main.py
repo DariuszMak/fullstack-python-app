819b227a5930f9ce5eaef7ed9951bdbba7cfe8f1
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-21 13:53:56 +0200

    move application to pyside_ui

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import threading
 from venv import logger
 
 from src import node_setup, pyside_setup
-from src.app import application
+from src.ui.pyside_ui import application
 from src.backend.api.app import run_api
 from src.helpers.logging_setup import logging_setup
 from src.ui.panel_ui.server import start_panel_in_background
