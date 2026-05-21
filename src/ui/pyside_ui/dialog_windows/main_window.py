3a6b192b2e2df9b47912cb49728e6adf40d1b06c
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-21 12:27:26 +0200

    autoformat

--- src/ui/pyside_ui/dialog_windows/main_window.py
+++ src/ui/pyside_ui/dialog_windows/main_window.py
@@ -6,9 +6,9 @@ from PySide6.QtCore import QEasingCurve, QEvent, QObject, QPropertyAnimation, Qt
 from PySide6.QtGui import QCloseEvent, QGuiApplication, QKeyEvent, QResizeEvent
 from PySide6.QtWidgets import QSystemTrayIcon
 
-from src.backend.openmeteo.gather import gather_data
 from src.backend.api.client.time_client import TimeClient
 from src.backend.api.models import ServerTimeResponse
+from src.backend.openmeteo.gather import gather_data
 from src.helpers.config.config import Config
 from src.helpers.style_loader import StyleLoader
 from src.ui.pyside_ui.clock_widget.view.clock_widget import ClockWidget
