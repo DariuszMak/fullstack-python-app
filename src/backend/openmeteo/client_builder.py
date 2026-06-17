78e620d8ef8773ac5657bc66bf3ffec20f100c79
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-17 09:38:41 +0200

    use another cache

--- src/backend/openmeteo/client_builder.py
+++ src/backend/openmeteo/client_builder.py
@@ -1,6 +1,5 @@
 import openmeteo_requests
 import requests_cache
-from requests_cache.backends.redis import RedisCache
 from retry_requests import retry
 
 
@@ -10,7 +9,10 @@ def build_openmeteo_client(
     retries: int = 5,
     backoff_factor: float = 0.2,
 ) -> openmeteo_requests.Client:
-    backend = RedisCache(host="localhost", port=6379)
-    cache_session = requests_cache.CachedSession(cache_name, backend=backend, expire_after=expire_after)
+    cache_session = requests_cache.CachedSession(
+        cache_name,
+        backend="sqlite",
+        expire_after=expire_after,
+    )
     retry_session = retry(cache_session, retries=retries, backoff_factor=backoff_factor)
     return openmeteo_requests.Client(session=retry_session)
