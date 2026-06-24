09f9235d4a519b8223f5aebd3380676f78e5c606
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-06-24 15:49:30 +0200

    update release

--- tasks/build_release.ps1
+++ tasks/build_release.ps1
@@ -10,7 +10,7 @@ $env:UV_ENV_FILE = ".dev.env" ;
 .\scripts\format_and_lint.ps1 ; 
 .\src\ui\react_ui\frontend\frontend_format_and_lint.ps1 ; 
 
-docker-compose run app sh -c "dos2unix thorough.env src/ui/react_ui/frontend/frontend_test.sh" ; 
+docker-compose run app sh -c "dos2unix thorough.env src/backend/openmeteo/places/places.json src/ui/react_ui/frontend/frontend_test.sh" ; 
 docker-compose run app sh -c "cd src/ui/react_ui/frontend && rm -rf node_modules package-lock.json && npm install && cd /app && uv sync --dev --locked --no-cache && chmod +x src/ui/react_ui/frontend/frontend_test.sh && src/ui/react_ui/frontend/frontend_test.sh && uv run pytest tests/ --cov=src"
 docker-compose run --rm --remove-orphans app sh -c "uv sync --dev --locked --no-cache && uv run pyinstaller --clean ./scripts/standalone_build_linux.spec && cp -r dist/* releases/linux/" ; 
 rm -r -fo .\dist, .\build, .\linux ; 
