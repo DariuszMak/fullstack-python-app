eddadaff3fcc9297a658ad80b9f784b86b4629f3
Dariusz <darekmakarewicz1994@gmail.com>
2026-08-03 08:58:11 +0200

    update scripts

--- tasks/build_release.ps1
+++ tasks/build_release.ps1
@@ -1,32 +1,29 @@
 docker-compose run --build app ; 
 
-uv python install 3.13 ; 
-uv python pin 3.13 ; 
-uv sync --dev --no-cache --locked ; 
+docker-compose run app sh -c "dos2unix thorough.env src/backend/openmeteo/places/places.json src/ui/react_ui/frontend/frontend_test.sh" ; 
+docker-compose run app sh -c "cd src/ui/react_ui/frontend && rm -rf node_modules package-lock.json && npm install && cd /app && uv sync --dev --locked --no-cache && chmod +x src/ui/react_ui/frontend/frontend_test.sh && src/ui/react_ui/frontend/frontend_test.sh && uv run pytest tests/ --cov=src"
+docker-compose run --rm --remove-orphans app sh -c "uv sync --dev --locked --no-cache && uv run pyinstaller --clean ./scripts/standalone_build_linux.spec && cp -r dist/* releases/linux/" ; 
+Remove-Item -r -fo .\dist, .\build, .\linux ; 
 
-.venv\Scripts\Activate.ps1 ; 
-$env:UV_ENV_FILE = ".dev.env" ; 
+
+Set-Location src\ui\react_ui\frontend ; 
+if (Test-Path node_modules) {
+    Remove-Item -Recurse -Force node_modules
+}
+Remove-Item package-lock.json ; 
+npm cache clean --force ; 
+npm install ; 
+Set-Location (git rev-parse --show-toplevel)
+
+uv sync --dev --no-cache --locked ; 
 
 .\scripts\format_and_lint.ps1 ; 
 .\src\ui\react_ui\frontend\frontend_format_and_lint.ps1 ; 
 
-docker-compose run app sh -c "dos2unix thorough.env src/backend/openmeteo/places/places.json src/ui/react_ui/frontend/frontend_test.sh" ; 
-docker-compose run app sh -c "cd src/ui/react_ui/frontend && rm -rf node_modules package-lock.json && npm install && cd /app && uv sync --dev --locked --no-cache && chmod +x src/ui/react_ui/frontend/frontend_test.sh && src/ui/react_ui/frontend/frontend_test.sh && uv run pytest tests/ --cov=src"
-docker-compose run --rm --remove-orphans app sh -c "uv sync --dev --locked --no-cache && uv run pyinstaller --clean ./scripts/standalone_build_linux.spec && cp -r dist/* releases/linux/" ; 
-rm -r -fo .\dist, .\build, .\linux ; 
-
-cd src\ui\react_ui\frontend ; 
-if (Test-Path node_modules) {
-    Remove-Item -Recurse -Force node_modules
-}
-del package-lock.json ; 
-npm cache clean --force ; 
-npm install ; 
-cd (git rev-parse --show-toplevel)
-
-.\src\ui\react_ui\frontend\frontend_test.ps1 ; 
-uv run pytest tests/ --cov=src -vv ;
-uv sync --no-dev --locked --no-cache ; 
 uv run pyinstaller --clean .\scripts\standalone_build_windows.spec ; 
-cp -r -fo .\dist\* .\releases\windows\ ; 
-rm -r -fo .\dist, .\build ; 
+
+Copy-Item -r -fo .\dist\* .\releases\windows\ ; 
+Remove-Item -r -fo .\dist, .\build ; 
+
+
+uv sync --dev --locked --no-cache ; 
