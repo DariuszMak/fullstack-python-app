209e7b08a3ae064d0c2a6579e7ffae6c0501ad72
Dariusz <darekmakarewicz1994@gmail.com>
2026-09-18 10:11:27 +0200

    update cors

--- src/backend/api/app.py
+++ src/backend/api/app.py
@@ -17,7 +17,9 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:8001",
+    "http://127.0.0.1:8001",
     "http://localhost:8002",
+    "http://127.0.0.1:8002",
 ]
 
 app.add_middleware(
