cc1c729156e11e1a3720d9beb436583a4789244b
Dariusz <darekmakarewicz1994@gmail.com>
2026-09-21 14:02:30 +0200

    add more commands

--- tasks/profile_dev_application.ps1
+++ tasks/profile_dev_application.ps1
@@ -1,4 +1,4 @@
-uv run scalene run .\src\main.py
+uv run scalene run --malloc-threshold 1 --cpu-percent-threshold 0 --profile-all .\src\main.py
 
 uv run scalene view --standalone
 Start-Process .\scalene-profile.html ; 
